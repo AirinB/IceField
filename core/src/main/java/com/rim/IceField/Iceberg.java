c9285a36b18d74a6689a388703237d8d3dc0ec35
Danil Stefaniuc <danilashtefan@gmail.com>
2020-03-28 16:55:02 +0200

    Iceberg class update. Please, pay attention to the constructor. I assume that parameters will be assigned randomly outside the class. Is it right?

--- core/src/main/java/com/rim/IceField/Iceberg.java
+++ core/src/main/java/com/rim/IceField/Iceberg.java
@@ -2,6 +2,7 @@ package com.rim.IceField;
 
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Iceberg {
     private boolean isStable;
@@ -18,106 +19,138 @@ public class Iceberg {
 
 public Iceberg(boolean isStable,int num,String type,int maxNumOfPlayers,boolean hasIgloo, int amountOfSnow, ItemBase item)
 {
-    //FINISH CONSTRUCTOR
+   //All parameters should be assigned randomly outside the class
 }
     public void setItem(ItemBase item)
     {
+        System.out.println("setItem()");
         this.item = item;
     }
     public ItemBase getItem()
     {
+
+        System.out.println("getItem()");
         return this.item;
     }
     public void setAmountOfSnow(int n)
     {
+
+        System.out.println("setAmountOfSnow()");
         this.amountOfSnow = n;
     }
 
     public int getAmountOfSnow()
     {
+        System.out.println("getAmountOfSnow()");
         return this.amountOfSnow;
     }
 
     public ArrayList<Iceberg> getNeighborIcebergs()
     {
+
+        System.out.println("getNeighborIcebergs()");
         return this.neighborIcebergs;
     }
 
     public ArrayList<PlayerBase> getCurrentPlayers()
     {
+
+        System.out.println("getCurrentPlayers()");
         return this.currentPlayers;
     }
 
 
     public ArrayList<PlayerBase> getDrowningPlayers()
     {
+
+        System.out.println("getDrowningPlayers()");
         return this.drowningPlayers;
     }
 
     public void setHasIgloo(boolean b)
     {
+
+        System.out.println("setHasIgloo()");
         this.hasIgloo = b;
     }
     public boolean getHasIgloo()
     {
+
+        System.out.println("getHasIgloo()");
         return this.hasIgloo;
     }
 
     public void setMaxNumOfPlayers(int n)
     {
+
+        System.out.println("setMaxNumOfPlayers()");
         this.maxNumOfPlayers = n;
     }
     public int getMaxNumOfPlayers()
     {
+        System.out.println("getMaxNumOfPlayers()");
         return maxNumOfPlayers;
     }
 public String getType()
 {
+    System.out.println("getType()");
     return type;
 }
 public void setType(String str)
 {
+    System.out.println("setType()");
     type = str;
 
 }
     public int getNum()
     {
+        System.out.println("getNum()");
         return num;
     }
     public void setNum(int n )
     {
+
+        System.out.println("setNum()");
         num = n;
     }
     public boolean getIsStable()
     {
-        return isStable;
+
+        System.out.println("getIsStable()");
+    return isStable;
     }
 
     public void setIsStable(boolean b)
     {
+
+        System.out.println("setIsStable()");
         isStable=b;
     }
 
  public void Add_currentPlayers(PlayerBase p)
  {
+     System.out.println("Add_currentPlayers()");
      currentPlayers.add(p);
      System.out.println("Player"+p.ID+" was added to the iceberg number: "+num);
  }
 
  public void Remove_currentPlayers(PlayerBase p)
  {
+     System.out.println("Remove_currentPlayers()");
      currentPlayers.remove(p);
      System.out.println("Player was removed from  the iceberg number: "+num);
  }
 
  public void Add_drowningPlayers(PlayerBase p)
  {
+     System.out.println("Add_drowningPlayers()");
      drowningPlayers.add(p);
      System.out.println("The "+p.ID+"has fallen in the water");
 
  }
  public void Remove_drowningPlayers(PlayerBase p)
  {
+     System.out.println("Remove_drowningPlayers()");
      drowningPlayers.remove(p);
      System.out.println("The player"+p.ID+" is saved. You are a hero!");
  }
