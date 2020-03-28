8e025a40758892d99c05f2f7602f8623f8ddbf85
Anna Hramova <33528244+annahramova97@users.noreply.github.com>
2020-03-28 17:05:09 +0100

    Updated classes Game,Map and some changes in Inventory

--- core/src/main/java/com/rim/IceField/Game.java
+++ core/src/main/java/com/rim/IceField/Game.java
@@ -3,28 +3,33 @@ package com.rim.IceField;
 import java.util.ArrayList;
 
 public class Game {
-    private Map map;
+    private static Map map;
 
     public Game(){
-        this.map = new Map();
+        map = new Map();
     }
 
     public Map getMap() {
         return map;
     }
 
-    public void setMap(Map map) {
-        this.map = map;
-    }
 
     public static void GameOver()
     {
-        System.out.println("Game Over!");
+
+        for (Iceberg ice: map.getIcebergs() ){
+            if ( (ice.getCurrentPlayers().size() == ice.getNum())  &&  (Inventory.isFlareGunAssembled()) )
+                System.out.println("Game Over!");
+
+        }
     }
 
     public void newGame(ArrayList<PlayerBase> players){
-        //map.generateItemsOnMap();
-        //finish
+
+        map.generateItemsOnMap(players);
+        for (PlayerBase player: players ){
+            player.turn();
+        }
 
         System.out.println("Game started!");
     }
--- core/src/main/java/com/rim/IceField/Map.java
+++ core/src/main/java/com/rim/IceField/Map.java
@@ -1,33 +1,106 @@
 package com.rim.IceField;
 
+import java.util.ArrayList;
+
 public class Map {
 
-    //public Blizzard blizzard;
-      private Iceberg iceberg;
-      private PlayerBase playerBase;
-      //public Inventory inventory;
+      //private Blizzard blizzard;
+      private ArrayList<Iceberg> icebergs;
+      private ArrayList<PlayerBase> players;
+      private ArrayList<ItemBase> items;
 
 
-    public Iceberg getIceberg() {
-        return iceberg;
+
+    public ArrayList<Iceberg> getIcebergs() {
+        return icebergs;
     }
 
-    public void setIceberg(Iceberg iceberg) {
-        this.iceberg = iceberg;
+    public ArrayList<PlayerBase> getPlayers() {
+        return players;
     }
 
-    public PlayerBase getPlayerBase() {
-        return playerBase;
+    public ArrayList<ItemBase> getItems() {
+        return items;
     }
 
-    public void setPlayerBase(PlayerBase playerBase) {
-        this.playerBase = playerBase;
+//    public Blizzard getBlizzard(){
+//        return blizzard;
+//    }
+
+    public void setIcebergs(ArrayList<Iceberg> icebergs) {
+        this.icebergs = icebergs;
     }
 
+    public void setItems(ArrayList<ItemBase> items) {
+        this.items = items;
+    }
+
+    public void setPlayers(ArrayList<PlayerBase> players) {
+        this.players = players;
+    }
+
+//    public void setBlizzard(Blizzard blizzard) {
+//        this.blizzard = blizzard;
+//    }
 
 
-    public void generateItemsOnMap(){
 
-      }
+    public void generateItemsOnMap(ArrayList<PlayerBase> players){
+
+          // - - - - - - - - - - - - - - - - - - - - - Blizzard - - - - - - - - - - - - - - - - - - - - -
+
+          //this.blizzard = new Blizzard();
+          //blizzard.Blow();
+
+         // - - - - - - - - - - - - - - - - - - - - - Items - - - - - - - - - - - - - - - - - - - - -
+
+         this.items = new ArrayList<ItemBase>();
+
+         ItemBase shovel = new ItemBase("shovel");
+         items.add(shovel);
+
+         ItemBase rope = new ItemBase("rope");
+         items.add(rope);
+
+         ItemBase food = new ItemBase("food");
+         items.add(food);
+
+         ItemBase flare = new ItemBase("flare");
+         items.add(flare);
+
+         ItemBase divingSuit = new ItemBase("divingSuit");
+         items.add(divingSuit);
+
+         ItemBase gun = new ItemBase("gun");
+         items.add(gun);
+
+         System.out.println("Items were generated!");
+
+
+
+
+          // - - - - - - - - - - - - - - - - - - - - - Icebergs - - - - - - - - - - - - - - - - - - - - -
+
+          this.icebergs =  new ArrayList<Iceberg>();
+
+          //Items have to be added later to create the test-cases
+          Iceberg first = new Iceberg(true, players.size(), "stable", 20, false, 1, null  );
+          icebergs.add(first);
+
+          Iceberg second = new Iceberg(true, 0, "instable", 3, false, 2, null  );
+          icebergs.add(second);
+
+          Iceberg third = new Iceberg(true, 0, "stable", 20, false, 3, null  );
+          icebergs.add(third);
+
+          Iceberg forth = new Iceberg(true, 0, "hole", 0, false, 2, null  );
+          icebergs.add(forth);
+
+          Iceberg fifth = new Iceberg(true, 0, "stable", 20, false, 1, null );
+          icebergs.add(fifth);
+
+          System.out.println("Icebergs were generated!");
+
+    }
 
 }
