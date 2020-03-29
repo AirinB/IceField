27c7d061bb36cdacfe5d2dc8fafdaa40be3b8928
andreea.moraru <andreeamoraru090@gmail.com>
2020-03-29 17:08:26 +0200

    Recovered Eskimo class

---
+++ core/src/main/java/com/rim/IceField/Eskimo.java
@@ -0,0 +1,18 @@
+package com.rim.IceField;
+
+public class Eskimo extends PlayerBase {
+    private boolean usedIgloo;
+
+    public Eskimo()
+    {
+        super();
+        this.tag = "Eskimo";
+        this.heatLevel = 5;
+
+    }
+
+    @Override
+    public void useSkill(Iceberg ice) {
+        System.out.println("An Igloo has been created!");
+    }
+}
