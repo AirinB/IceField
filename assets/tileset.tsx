e9976cc493c7b5d634ae06b7766b9faef6e764dd
Ghaith Ahmad <58469843+RainZoneO2@users.noreply.github.com>
2020-05-14 20:38:50 +0200

    Centered map, added snow, testing out snow removal, and added background

--- assets/tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.4" name="tileset" tilewidth="32" tileheight="32" spacing="1" tilecount="3" columns="3">
- <image source="tileset.png" width="98" height="32"/>
-</tileset>
--- core/src/main/java/com/rim/IceField/MapGUI.java
+++ core/src/main/java/com/rim/IceField/MapGUI.java
@@ -1,25 +1,18 @@
 package com.rim.IceField;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
-import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import org.mini2Dx.core.graphics.Graphics;
-import org.mini2Dx.core.graphics.Sprite;
 
 public class MapGUI {
-    private final int TILE_SIZE = 32;               //Defines the size of the tile in pixels
+    private final int TILE_SIZE = 48;               //Defines the size of the tile in pixels
     private Map map;
-    Texture tile1;
-    Texture tile2;
-    Texture tile3;
-    Sprite sprite;
+    Texture iceberg;
+    Texture water;
+    Texture snow;
+
     private SpriteBatch sBatch;
-    private TiledMap tMap;
-    private OrthogonalTiledMapRenderer renderer;
 
     public MapGUI(Map map) {
         this.map = map;
@@ -28,14 +21,10 @@ public class MapGUI {
 
 
     public void initialise() {
-        //tMap = new TmxMapLoader().load("assets/map.tmx");
-        //tiles = new Texture("assets/iceberg32.png","assets/water.jpg","assets/snow.png")[3];
-        tile1 = new Texture("assets/iceberg32.png");
-        tile2 = new Texture("assets/water.jpg");
-        tile3 = new Texture("assets/snow.png");
+        iceberg = new Texture("assets/iceberg.png");
+        water = new Texture("assets/water.jpg");
+        snow = new Texture("assets/snow.png");
 
-        sprite = new Sprite(tile1, 100, 100, 100, 100);
-        sprite.setPosition(40, 40);
         sBatch = new SpriteBatch();
     }
 
@@ -44,23 +33,16 @@ public class MapGUI {
     }
 
     public void render(Graphics g) {
-        //Gdx.gl.glClearColor(0,0,0,1);
-        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        //renderer.render();
         sBatch.begin();
-
+        sBatch.draw(water, 0 ,0, 640, 480);
         for (int Y = 0; Y < map.getMAP_WIDTH(); Y++) {
             for (int X = 0; X < map.getMAP_HEIGHT(); X++) {
                 if (map.Icebergs[Y][X].getIsStable()) {
-                    //g.drawTexture(tile1, X * TILE_SIZE, Y * TILE_SIZE);
-                    sBatch.draw(tile1,X * TILE_SIZE, Y * TILE_SIZE, 32, 32);
+                    sBatch.draw(iceberg,X * TILE_SIZE + 88, Y * TILE_SIZE, 48, 48);
                 }
-                else if (map.Icebergs[Y][X].getAmountOfSnow() > 0) {
-                    //g.drawTexture(tile3, X * TILE_SIZE, Y * TILE_SIZE);
-                    sBatch.draw(tile3, X * TILE_SIZE, Y * TILE_SIZE, 32, 32);
+                if (map.Icebergs[Y][X].getAmountOfSnow() > 0) {
+                    sBatch.draw(snow, X * TILE_SIZE + 88, Y * TILE_SIZE, 48, 48);
                 }
-                /*else if (map.Icebergs[Y][X].)
-                    g.drawTexture(tiles[1], X * TILE_SIZE, Y * TILE_SIZE);*/
             }
         }
         sBatch.end();
